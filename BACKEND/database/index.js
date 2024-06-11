98b2eb41d1e61bcb3def7d5d75698ee7eec4eebb
davidprhn <davidprhn@gmail.com>
2024-06-11 20:24:46 +0100

    initial commit

---
+++ BACKEND/database/index.js
@@ -0,0 +1,11 @@
+const { Sequelize } = require('sequelize')
+
+const connection = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+}
+);
