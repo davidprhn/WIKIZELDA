9869d415836480216eae451f1ca4cd5771e290d2
davidprhn <davidprhn@gmail.com>
2024-06-11 20:44:53 +0100

    connection to db succesful

--- BACKEND/database/index.js
+++ BACKEND/database/index.js
@@ -8,4 +8,18 @@ const connection = new Sequelize(
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
 }
-);
+)
+
+const checkDB = async () => {
+    try {
+    await connection.authenticate()
+     console.log('Connection has been established successfully.')
+    } catch (error) {
+     console.error('Unable to connect to the database:', error)
+    }
+}
+
+module.exports = {
+    connection,
+    checkDB
+}
