599bdba3d27538fc2c514ebc2237e9139a253af8
andrearmedina <andreamedina@hotmail.co.uk>
2024-06-26 19:13:49 +0100

    home frontend fixes

--- FRONTEND/src/pages/Home/Home.jsx
+++ FRONTEND/src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
-import './Home.css'
+import './Home.css';
+import welcomePicture from '../Home/multimedia/welcome.png';
 
 function Home() {
   return (
     <div className="home-container">
-      <h1>HOME</h1>
+      <p>
+        Ready to jump into a new adventure ? <br />
+        <br />
+        You just found the perfect page to comment your favorite Zelda games with gamers from all around the world ! <br />
+      </p>
+      <img src={welcomePicture} alt="welcomePicture" />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
